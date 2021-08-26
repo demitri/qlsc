7194e1e4fdb94f8aefd5a454616774504abe2581
Demitri Muna <github@demitri.com>
2021-08-26 17:24:14 -0400

    Fixing missing include statements from build process. Bumping to v1.0.4.

--- source/setup.py
+++ source/setup.py
@@ -100,6 +100,7 @@ setup(
     ext_modules=[c_extension], # alternative: cythonize(etc), needs "from Cython.Build import cythonize"
     include_dirs=['q3c'],
     packages=setuptools.find_packages(), #['qlsc'],
+    include_package_data = True, # add files specified in MANIFEST.in, specifically header files
     python_requires='>=3.6'
 )
 #    cmdclass={"build_ext": build_ext_subclass}
